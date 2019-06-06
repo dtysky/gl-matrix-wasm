46c88a53ef9bfbcc921392128795aef444b0556b
瞬光 <shunguang.dty@alibaba-inc.com>
2019-06-06 18:34:59 +0800

    Add Matrix2

--- trans/trans.js
+++ trans/trans.js
@@ -2,9 +2,31 @@
 /**
  * @File   : trans.js
  * @Author : dtysky (dtysky@outlook.com)
- * @Date   : 6/6/2019, 6:32:58 PM
+ * @Date   : 6/6/2019, 5:21:14 PM
  * @Description:
  */
+// out
+// out, a
+// out, a, b
+// a
+// b
+// a, b
+
+
+// ===
+// return out;
+
+// let x = a.0, a1 = a.1, ...;
+
+// export function
+
+// m(dd)
+// x, y, z
+
+// new glMatrix.ARRAY_TYPE(d)
+
+// export const sqrLen = squaredLength;
+
 const glMatrixPath = '';
 
 
